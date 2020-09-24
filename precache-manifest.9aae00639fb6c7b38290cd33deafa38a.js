1cab83f28a4b70570b5052983580f236a38797df
andy1729 <ujjawalanand1729@gmail.com>
2020-09-24 13:28:16 +0530

    Updates

---
+++ precache-manifest.9aae00639fb6c7b38290cd33deafa38a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3538112feb0bd92b3547630c8edfe66",
+    "url": "/andy1729/react-quiz/index.html"
+  },
+  {
+    "revision": "b4dca690e3824369a51d",
+    "url": "/andy1729/react-quiz/static/css/main.a1fa5f91.chunk.css"
+  },
+  {
+    "revision": "c1b840b9e62737974294",
+    "url": "/andy1729/react-quiz/static/js/2.b24296e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andy1729/react-quiz/static/js/2.b24296e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dca690e3824369a51d",
+    "url": "/andy1729/react-quiz/static/js/main.64e697c0.chunk.js"
+  },
+  {
+    "revision": "e59da1dc471425aa71f8",
+    "url": "/andy1729/react-quiz/static/js/runtime-main.2075b1b4.js"
+  },
+  {
+    "revision": "fa9b4dd481943c47787431c5a6305192",
+    "url": "/andy1729/react-quiz/static/media/icon-check.fa9b4dd4.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/andy1729/react-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
